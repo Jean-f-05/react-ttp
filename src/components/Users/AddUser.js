8af0ef890f68fd47c72102b15e70b2f90a3f8d25
John Francis <jean_f_05@hotmail.com>
2022-06-17 10:51:58 +0100

    added Card wrapper

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const addUserHandler = (e) => {
@@ -6,14 +8,16 @@ const AddUser = (props) => {
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text"></input>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
 
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number"></input>
-      <button type="submit">Add User</button>
-    </form>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"></input>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
